1db30031fb9956114d7c275f3b0496a5f06015ff
Sutton <suttondemlong@gmail.com>
2020-12-10 17:47:31 -0500

    Fill in about me content

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -6,7 +6,22 @@ function About(props) {
     <div>
       <Header/>
       <h2 className="page-title">About Me</h2>
-      <p>This is who I am</p>
+      <div classname="about-all">
+        <img
+          className="my-photo"
+          src="https://github.com/suttondemlong/portfolio/blob/main/src/assets/IMG_8767.JPG?raw=true"></img>
+        <div className="about-content">
+          <p className="about-text">I am a junior developer with a background in fine arts and production based woodworking.
+          As a production manager at LANNINGSMITH Studios, I learned how to navigate teams of unique individuals,
+          balance client expectations with production realities and problem solve/pivot with a looming deadline.
+          Through my art practice, I have developed a thirst for understanding how the infinite iterations of
+          people’s perspectives clash, synchronize and ultimately weave together the fabric of our society.
+          This passion combined with my love for building fuels my practice as a developer. It is the reason
+          why I excel as a team member as well as a project lead and it is what inspires me to use software
+          engineering to create a positive impact on the world.</p>
+          <a className="link" href="http://sutton-demlong.squarespace.com/">Art Practice</a>
+        </div>
+      </div>
     </div>
   );
 }
