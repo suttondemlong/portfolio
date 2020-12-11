8f95bbd89e0f1b88540c6810ec00a02e955a14f1
Sutton <suttondemlong@gmail.com>
2020-12-11 10:15:55 -0500

    Add source photos for projects

--- src/screens/Projects.jsx
+++ src/screens/Projects.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Header from '../components/Header'
+import Footer from '../components/Footer'
 import '../App.css';
 
 function Projects(props) {
   return (
-    <div>
+    <div className="projects-page">
       <Header />
       <h2 className="page-title">PROJECTS</h2>
+      <div className="projects-container">
+        <div className="project-tile" id="project-1">View Details</div>
+        <div className="project-tile" id="project-2">View Details</div>
+        <div className="project-tile" id="project-3">View Details</div>
+      </div>
+      <Footer/>
     </div>
   );
 }
