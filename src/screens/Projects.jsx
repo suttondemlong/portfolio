ecf498bb010edfc25aa777d5b49212d36883b3be
Sutton <suttondemlong@gmail.com>
2020-12-11 16:00:59 -0500

    Update Thought Vomit Image

--- src/screens/Projects.jsx
+++ src/screens/Projects.jsx
@@ -9,9 +9,36 @@ function Projects(props) {
       <Header />
       <h2 className="page-title">PROJECTS</h2>
       <div className="projects-container">
-        <div className="project-tile" id="project-1">View Details</div>
-        <div className="project-tile" id="project-2">View Details</div>
-        <div className="project-tile" id="project-3">View Details</div>
+        <div className="project-tile" id="project-1">
+          <div className="tile-hover">
+            <a href="https://suttondemlong.github.io/Parks-ALERT-/">View Project</a>
+            <a href='https://github.com/suttondemlong/Parks-ALERT-'>
+              <img
+                className="project-icon"
+                src='https://raw.githubusercontent.com/suttondemlong/portfolio/37db0898f2884dce40db2875d3c2d2b726dc190c/src/assets/github.svg' alt='GitHub' />
+            </a>
+          </div>
+        </div>
+        <div className="project-tile" id="project-2">
+          <div className="tile-hover">
+            <a href="https://objective-williams-628b36.netlify.app/">View Project</a>
+            <a href='https://github.com/suttondemlong/paint-by-grid'>
+              <img
+                className="project-icon"
+                src='https://raw.githubusercontent.com/suttondemlong/portfolio/37db0898f2884dce40db2875d3c2d2b726dc190c/src/assets/github.svg' alt='GitHub' />
+            </a>
+          </div>
+        </div>
+        <div className="project-tile" id="project-3">
+          <div className="tile-hover">
+            <a href="https://thought-vomit.netlify.app/">View Project</a>
+            <a href='https://github.com/suttondemlong/Thought-Vomit'>
+              <img
+                className="project-icon"
+                src='https://raw.githubusercontent.com/suttondemlong/portfolio/37db0898f2884dce40db2875d3c2d2b726dc190c/src/assets/github.svg' alt='GitHub' />
+            </a>
+          </div>
+        </div>
       </div>
       <Footer/>
     </div>
