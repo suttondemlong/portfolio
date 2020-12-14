04672780607d83d29fb820f0b283eb4161788fb4
Sutton <suttondemlong@gmail.com>
2020-12-14 12:04:43 -0500

    Mobile styling for projects page and header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,14 +5,25 @@ import './Header.css';
 function Header(props) {
   return (
     <div className="header">
-      <Link className='my-name' to='/'>SUTTON DEMLONG</Link>
-      <Link id="header-projects" to="/projects">PROJECTS</Link>
-      <a
-        href="https://drive.google.com/file/d/164HD2MhgaOd8PqIUPmj0UELFq_A8-Q5a/view?usp=sharing"
-        id="header-resume">
-        RESUME
-      </a>
-      <Link id="header-about" to='/about'>ABOUT</Link>
+      <div className="header-title">
+        <Link className='my-name' to='/'>SUTTON DEMLONG</Link>
+      </div>
+      <div className="header-options">
+        <Link
+          className="header-option"
+          id="header-projects"
+          to="/projects">PROJECTS</Link>
+        <a
+          className="header-option"
+          href="https://drive.google.com/file/d/164HD2MhgaOd8PqIUPmj0UELFq_A8-Q5a/view?usp=sharing"
+          id="header-resume">
+          RESUME
+        </a>
+        <Link
+          className="header-option"
+          id="header-about"
+          to='/about'>ABOUT</Link>
+      </div>
     </div>
   );
 }
