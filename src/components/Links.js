b42394841b296176308c5f52a1c791be49c0ae5d
Sutton Demlong <suttondemlong@gmail.com>
2021-07-21 12:47:23 -0400

    Update resume link

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,1 +1,1 @@
-export const resumeLink = "https://drive.google.com/file/d/1zcwhwhwQGRiaqUAQiZjB98qSmxPdyy46/view?usp=sharing"
+export const resumeLink = "https://drive.google.com/file/d/1VGhHT2Fc02C4nnhm6VjxHUeKVox6key3/view?usp=sharing"
